50e2e7d5ac7bf47936586db01d592a653845020e
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-05-17 17:34:07 -0300

    treinando js

---
+++ comparações.js
@@ -0,0 +1,17 @@
+//== comparação implicita
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+
+//typeof
+console.log(typeof);
+
+//== compara só o valor
+// === compara o valor e o tipo de dado
+//Atribuição de adição	x + y
+//Atribuição de subtração	x - y
+//Atribuição de multiplicação	x * y
+//Atribuição de divisão	x / y
