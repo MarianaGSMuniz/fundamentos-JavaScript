426b89b8dd3381fb6e71e87daf320a42edc9ae9c
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-05-17 16:39:39 -0300

    erros e console.log

---
+++ erros.js
@@ -0,0 +1,6 @@
+// erros 
+const numero = ;
+console.log(numero; console.log(minhaVar):
+
+        //correto
+        const numero = 0; console.log(numero);
---
+++ log.js
@@ -0,0 +1,10 @@
+// log -> registro
+const minhaVar = true;
+console.log(123);
+console.log("eu sou um texto");
+console.log(minhaVar);
+
+//console.error quando pré definimos um erro!
+//tratamento de erro!
+
+console.error('usuário inválido');
