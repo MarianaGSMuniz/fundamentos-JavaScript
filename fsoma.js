fc399729f138485edba496ba1c6c319a24b591b2
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-05-18 11:45:59 -0300

    continuação das aulas

---
+++ fsoma.js
@@ -0,0 +1,6 @@
+function soma() {
+    return 3 + 4;
+
+
+}
+console.log(soma())
---
+++ função.js
@@ -0,0 +1,11 @@
+let x = "";
+console.log(x);
+x = "oi";
+
+
+function mostraTexto(texto) {
+    console.log(texto)
+}
+
+mostraTexto(Olá)
+mostraTexto(Tudo bem)
