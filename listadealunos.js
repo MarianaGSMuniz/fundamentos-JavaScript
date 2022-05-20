cbd1c10178dd65beb13cfbbbdd9c9e8f25b70b40
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-05-20 09:31:56 -0300

    dividindo com splice

---
+++ listadealunos.js
@@ -0,0 +1,10 @@
+const nome = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'Andre',
+    'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinicius', 'Renan', 'Renata', 'Daisy', 'Camilo'
+]
+const salaA = nome.slice(0, nomes.length / 2)
+const salaB = nome.slice(nomes.length / 2)
+
+
+console.log('Alunos da sala A: ${salaA}')
+
+console.log('Alunos da sala B: ${salaB}')
---
+++ removernota.js
@@ -0,0 +1,5 @@
+const notas = [10, 7, 8, 5, 10]
+notas.pop()
+console.log()
+let media = (notas[0] + notas[1] + notas[2] + notas[3] / notas.legth)
+console.log('A media é $ {média}')
